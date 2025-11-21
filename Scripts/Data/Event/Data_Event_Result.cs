194ef6b7b70cb7d9a0a473f88bccb3e76faf2f90
nodong91 <nodong91@gmail.com>
2025-11-21 19:32:11 +0900

    이벤트 데이터 변경

--- Scripts/Data/Event/Data_Event_Result.cs
+++ Scripts/Data/Event/Data_Event_Result.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data_Event_Result", menuName = "Scriptable Objects/Data_Event_Result")]
 public class Data_Event_Result : Data_Event
 {
+    [Header(" [ 보상 아이템 ]")]
     public Data_ItemList itemList;// 보상 아이템
+    [Header(" [ 엔피씨 대화 ]")]
     public Data_NPC npcData;// 엔피씨
     public string dialogID;// 엔피씨 대화 넘버
+    [Header(" [ 돈 추가 ]")]
     public int addMoney;// 돈 추가
 }
